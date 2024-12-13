18a599b99b2ed414f8fa91ac5b7ccfdf8ec4f1c6
Joseph <langj6757@gmail.com>
2024-12-12 21:45:02 -0800

    Setup basic auth function structure

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,7 @@
+export const signup = (req, res) => {
+  res.send("signup");
+};
+
+export const login = () => {};
+
+export const logout = () => {};
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 import express from "express";
-
+import authRoutes from "./routes/auth.route.js";
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use("/api/v1/auth", authRoutes);
