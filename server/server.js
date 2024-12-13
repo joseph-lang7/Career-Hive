97d3e6d729d903bbeeb2f2a96e3b3c0703df1bfd
Joseph <langj6757@gmail.com>
2024-12-13 15:37:33 -0800

    Connect database

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  connectDB();
 });
 
 app.use("/api/v1/auth", authRoutes);
