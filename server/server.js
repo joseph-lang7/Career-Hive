89552895ff8dbd6fa302c4808256e3ff740116fb
Joseph <langj6757@gmail.com>
2024-12-13 16:02:27 -0800

    Create user model

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json()); // parse JSON request body
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
